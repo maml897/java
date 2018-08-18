15ae6a01d622e700a370d90b19b4350defcb3f67
maml <maml897@163.com>
2018-08-18 11:33:12 +0800

    ok

--- src/main/java/jingdu/BigDecimalError.java
+++ src/main/java/jingdu/BigDecimalError.java
@@ -2,9 +2,12 @@ package jingdu;
 
 import java.math.BigDecimal;
 
+import zutils.FormatUtils;
+
 public class BigDecimalError {
 
 	public static void main(String[] args) {
+		System.out.println(FormatUtils.float2String(new BigDecimal("172").floatValue())); //多了.0
 		System.out.println(new BigDecimal("172").floatValue()); //多了.0
 	}
 }
