f9a7c7a823ce9e32f9c4ed5cd0f59a621b65b405
maml <maml897@163.com>
2018-08-18 10:55:54 +0800

    ok

---
+++ src/main/java/jingdu/Test.java
@@ -0,0 +1,18 @@
+package jingdu;
+
+/**
+ * 该例子说明需要double参数的不能随意传float类型
+ * @author maml
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		float f=172.525f;
+		test(f);
+	}
+	
+	private static float test(double d){
+		System.out.println(String.valueOf(d));
+		return 0;
+	}
+}
