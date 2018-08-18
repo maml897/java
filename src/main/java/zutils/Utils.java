e832ac87c3ccfb001fd0874eb1e308d2c3a50cdd
maml <maml897@163.com>
2018-08-18 09:29:51 +0800

    ok

--- src/main/java/zutils/Utils.java
+++ src/main/java/zutils/Utils.java
@@ -33,6 +33,41 @@ public class Utils {
 		}
 	}
 	
+	/**
+	 * 首字母小写
+	 * @param s
+	 * @return
+	 */
+	public static String toLowerCaseFirst(String s)
+	{
+		if (Character.isLowerCase(s.charAt(0)))
+			return s;
+		else
+			return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+	}
+	
+	/**
+	 * 获取一个列表中的数据，如果列表长度不够返回默认值
+	 * @param list
+	 * @param i
+	 * @param defaultObject
+	 * @return
+	 */
+	@SafeVarargs
+	public static <U> U getListIndex(List<U> list, int i, U... us)
+	{
+		if (list.size() > i)
+		{
+			return list.get(i);
+		}
+		if (us.length > 0)
+		{
+			return us[0];
+		}
+		return null;
+	}
+	
+	
 	public static void main(String[] args) {
 		List<Float> list = Arrays.asList(0f, 5f, 10f, 15f, 20f);
 
