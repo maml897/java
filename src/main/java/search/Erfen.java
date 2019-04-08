d8451a1a43ff6d8a6779629517b3028adcfd6306
maml <maml897@163.com>
2019-04-08 08:53:26 +0800

    ok

--- src/main/java/search/Erfen.java
+++ src/main/java/search/Erfen.java
@@ -1,3 +1,4 @@
+package search;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
--- src/main/java/search/binarysearchKey/Main.java
+++ src/main/java/search/binarysearchKey/Main.java
@@ -1,4 +1,4 @@
-package binarysearchKey;
+package search.binarysearchKey;
 
 import java.util.ArrayList;
 import java.util.Collection;
